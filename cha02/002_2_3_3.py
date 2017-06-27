f7b27e53fe8cb6f6a46bd35616db4c6c0092d2ac
pooi <ldayou@naver.com>
2017-06-27 09:07:51 -0400

    Add code   * learn chapter 2

---
+++ cha02/002_2_3_3.py
@@ -0,0 +1,36 @@
+import numpy as np
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+for i in range(2):
+    for j in range(2):
+        print("AND : ", i, ", ", j, " : ", AND(i, j))
+        print("NAND : ", i, ", ", j, " : ", NAND(i, j))
+        print("OR : ", i, ", ", j, " : ", OR(i, j))
