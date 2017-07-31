009c550427197e43e4005221d5756ec0cc4f5c65
pooi <ldayou@naver.com>
2017-07-31 09:23:25 -0400

    Add code   * Change directory name   * learn 3.5.1 ~ 3.6.1

---
+++ ch03/003_3_5_2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([1010, 1000, 990])
+print(np.exp(a) / np.sum(np.exp(a)))
+
+c = np.max(a)
+print(a-c)
+
+print(np.exp(a-c) / np.sum(np.exp(a-c)))
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
